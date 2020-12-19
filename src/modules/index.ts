42d67078cd3e8fb2ba1afc11618217a35e683627
Saemii <wrfg12@gmail.com>
2020-12-19 12:12:57 +0900

    Set Redux & required package

---
+++ src/modules/index.ts
@@ -0,0 +1,8 @@
+// rootReducer 작성
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({});
+
+export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
